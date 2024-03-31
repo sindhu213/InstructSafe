793fa4900c6e34df53c3f14c02eb7d946e5f8b88
Sindhu <ksindhu571@gmail.com>
2024-04-01 00:09:58 +0530

    Update Button.tsx

--- components/Button.tsx
+++ components/Button.tsx
@@ -7,7 +7,8 @@ export default function Button() {
         setCount(count => count + 1)
     }
     return (
-        <button onClick={handleClick} className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+        <button onClick={handleClick} className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 
+            hover:border-transparent rounded">
             You pressed me {count} times!
         </button>
     );
